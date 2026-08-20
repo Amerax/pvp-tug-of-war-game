6ece568d19d6159f42dd33d4426c833e94c9ea0b
Raiyan K <96327738+Amerax@users.noreply.github.com>
2026-08-20 14:28:20 -0400

    Implement player movement and win conditions

--- main.ino
+++ main.ino
@@ -21,3 +21,56 @@ void setup() {
   tone(13, 1000, 300);
   delay(500);
 }
+
+void loop() {
+  if (digitalRead(2) == LOW) {
+    pos = pos - 5;
+    tone(13, 600, 50);
+    delay(100);
+  }
+
+  if (digitalRead(3) == LOW) {
+    pos = pos + 5;
+    tone(13, 800, 50);
+    delay(100);
+  }
+
+  oled.clearDisplay();
+
+  oled.setTextSize(1);
+  oled.setCursor(5, 5);
+  oled.print("P1");
+
+  oled.setCursor(108, 5);
+  oled.print("P2");
+
+  oled.drawLine(10, 35, 118, 35, WHITE);
+  oled.drawLine(64, 28, 64, 42, WHITE);
+  oled.fillCircle(pos, 35, 5, WHITE);
+
+  oled.display();
+//p1 win
+  if (pos <= 10) {
+    oled.clearDisplay();
+    oled.setTextSize(2);
+    oled.setCursor(15, 25);
+    oled.print("P1 WINS!");
+    oled.display();
+
+    tone(13, 1200, 500);
+    delay(2000);
+
+    pos = 64;
+  }//p2 win
+  if (pos >= 118) {
+    oled.clearDisplay();
+    oled.setTextSize(2);
+    oled.setCursor(15, 25);
+    oled.print("P2 WINS!");
+    oled.display();
+
+    tone(13, 1500, 500);
+    delay(2000);
+
+    pos = 64;
+  }
